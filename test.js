8701eaa299736aa972d5c369b745fdf42f2d8e6e
Seth Lakowske <lakowske@gmail.com>
2015-02-07 22:00:26 -0600

    live stream db

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ var http      = require('http');
 var level     = require('level');
 var through   = require('through');
 
-test('can pipe through http', function(t) {
+test('can pipe to capture', function(t) {
 
     //open the request logs db
     var db = level('test.db', { encoding: 'json' });
@@ -21,6 +21,8 @@ test('can pipe through http', function(t) {
         })
         capture.on('end', function() {
             console.log(result);
+            console.dir(db);
+            db.close()
             t.end();
         });
 
@@ -29,27 +31,115 @@ test('can pipe through http', function(t) {
 
 })
 
-/*
+test('can pipe through http', function(t) {
+    var db = level('test2.db', { encoding: 'json' });
+    var requestLogger = require('./')(db);
+    var requests      = requestLogger.requests();
+
     var server = http.createServer(function(req, res) {
-        requests(req, res);
+        res.statusCode = 200;
+        var count = 10;
+        var n     = 0;
+        var rate  = 20; // rate / 1000 = times per second
+        var timerId = setInterval(function() {
+            res.write('Hello Friend');
+            n += 1;
+            if (n >= count) {
+                res.write('Goodbye Friend');
+                res.end();
+                clearInterval(timerId);
+            }
+        }, rate);
+
+
+        //requests(req, res);
     }).listen(3322);
 
-    var req        = http.request('http://localhost:3322');
+    var options = {
+        host : 'localhost',
+        port : 3322,
+        path : '/test',
+        keepAlive : true
+    }
+
+    var req = http.request(options, function(res) {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+        });
+        res.on('end', function() {
+            console.log('ending test');
+            server.close();
+            t.end();
+        })
+    });
+
+    req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+    });
+
     req.end();
+})
 
-    req.on('connect', function(res, socket, head) {
-        console.log('got connected');
-        socket.write('GET / HTTP/1.1\r\n' +
-                     'Host: localhost:3322\r\n' +
-                     'Connection: close\r\n' +
-                     '\r\n');
-        socket.on('data', function(chunk) {
-            console.log('hi');
-            console.log(chunk.toString())
-        })
-        socket.on('end', function() {
-            console.log('req socket closed');
-        })
-    })
-*/
+test('can live stream db via http', function(t) {
+    var db = level('test3.db', { encoding: 'json' }, function(err, db) {
 
+        var server = http.createServer(function(req, res) {
+            res.statusCode = 200;
+            var count = 100;
+            var n     = 0;
+            var rate  = 2000; // rate / 1000 = times per second
+            var timerId = setInterval(function() {
+                db.put(n, 'kalinx');
+                n += 1;
+                if (n >= count) {
+                    res.end();
+                    clearInterval(timerId);
+                }
+            }, rate);
+
+            var dbstream = require('level-live-stream')(db);
+
+            dbstream.on('data', function(data) {
+                res.write(JSON.stringify(data) + '\n');
+            });
+
+            dbstream.on('end', function() {
+                res.end();
+            })
+
+            //requests(req, res);
+        }).listen(3322);
+
+        var options = {
+            host : 'localhost',
+            port : 3322,
+            path : '/test',
+            keepAlive : true
+        }
+
+        var req = http.request(options, function(res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('BODY: ' + chunk);
+            });
+            res.on('end', function() {
+                console.log('ending test');
+                server.close();
+                t.end();
+            })
+        });
+
+        req.on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+        });
+
+        req.end();
+
+    });
+
+})
