2ad330658c75696352616dfb1ae39ca6bd592d59
Seth Lakowske <lakowske@gmail.com>
2015-03-12 00:37:14 -0500

    end response

--- index.js
+++ index.js
@@ -76,6 +76,8 @@ RequestLogger.prototype.requests = function() {
             parseify.on('data', function(dbrequest) {
                 var millis = new Date().getTime();
                 self.db.put(millis, dbrequest);
+                res.write(data);
+                res.end();
             });
         }
     }
